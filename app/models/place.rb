929896b46896128da72ded58e3d91cbf8c2c4abd
OscarVegaV <os_ar1@hotmail.com>
2021-05-09 20:10:24 -0600

    fixed bugs, view models and controllers

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,6 @@
 class Place < ApplicationRecord
-  has_many :pictures
-  has_many :comments, as: :commentable
+  has_many :pictures, dependent: :destroy
+  has_many :comments, as: :commentable, dependent: :destroy
 
   geocoded_by :address
   after_validation :geocode, if: :address_changed?

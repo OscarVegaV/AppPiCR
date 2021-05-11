5a27e62d30cbb29f7c452a9dfb7e52a95c3537e9
OscarVegaV <os_ar1@hotmail.com>
2021-05-11 10:21:25 -0600

    working in the gem acts as favorito something to fixed

--- app/models/place.rb
+++ app/models/place.rb
@@ -5,4 +5,7 @@ class Place < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
   validates :name, presence: true
+
+  acts_as_favoritable
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   resources :places do
     resources :comments
     resources :pictures
+    member do
+      post 'toggle_favorite', to: "places#toggle_favorite"
+    end
   end
 
   resources :pictures, only:[:show, :index, :edit, :destroy] do
@@ -10,13 +13,15 @@ Rails.application.routes.draw do
 
     member do
       delete 'photo', to: 'pictures#delete_photo', as: 'photo'
-      end
+      post 'toggle_favorite', to: "pictures#toggle_favorite"
+    end      
   end
 
 
   resources :comments do
     resources :comments
   end
+  
   devise_for :users
   
   root "places#index"
